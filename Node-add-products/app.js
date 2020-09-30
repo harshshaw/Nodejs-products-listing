1bf12025e0ccb5e6f742cefe36f930fcfd64be2d
harshshaw <harshshaw5@gmail.com>
2020-09-30 19:02:12 +0530

    changes

--- Node-add-products/app.js
+++ Node-add-products/app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const errorController = require("./controllers/error");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,8 +17,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController);
 
 app.listen(3000);

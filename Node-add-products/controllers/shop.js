daa8645827ed185d69daba4128b59bab331d5c02
harshshaw <harshshaw5@gmail.com>
2020-10-04 18:11:30 +0530

    add to cart model added

--- Node-add-products/controllers/shop.js
+++ Node-add-products/controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product");
-
+const Cart = require("../Models/cart");
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
@@ -36,6 +36,14 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
--- Node-add-products/routes/shop.js
+++ Node-add-products/routes/shop.js
@@ -16,6 +16,8 @@ router.get("/products/:productId", shopController.getProduct); //colon here indi
 
 router.get("/cart", shopController.getCart);
 
+router.post("/cart", shopController.postCart);
+
 router.get("/orders", shopController.getOrders);
 
 router.get("/checkout", shopController.getCheckout);
